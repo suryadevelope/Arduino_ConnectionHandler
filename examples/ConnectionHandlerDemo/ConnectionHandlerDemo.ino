8bb7e68188d540c15bc0632e980c0581598e8e9f
suryadevelope <59243908+suryadevelope@users.noreply.github.com>
2022-09-09 19:09:34 +0530

    Update ConnectionHandlerDemo.ino

--- examples/ConnectionHandlerDemo/ConnectionHandlerDemo.ino
+++ examples/ConnectionHandlerDemo/ConnectionHandlerDemo.ino
@@ -2,14 +2,14 @@
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
 
