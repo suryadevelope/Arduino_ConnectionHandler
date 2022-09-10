e0f458403527ee8e8654f5106bc84617caf62052
suryadevelope <59243908+suryadevelope@users.noreply.github.com>
2022-09-10 14:05:29 +0530

    Update ConnectionHandlerDemo.ino

--- examples/ConnectionHandlerDemo/ConnectionHandlerDemo.ino
+++ examples/ConnectionHandlerDemo/ConnectionHandlerDemo.ino
@@ -5,6 +5,7 @@ void setup() {
   pinMode(13, OUTPUT);
 }
 
+
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
